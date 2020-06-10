8b684b02b00cfd2cf4394aca7fe999f0fcf3a0dc
Jose Villalta <josemiguelvillaltah@gmail.com>
2020-06-10 12:16:05 -0500

    Fin Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
+    courses = COURSES;
 
+    startDay = new Date(2000, 0, 1);
+
+    title = COURSES[1].description;
+
+    price = 9.992324;
+
+    rate = 0.67;
+
+    course = COURSES[1];
+
+    onCardClicked() {
+        console.log('APP component - Course card clicked');
+    }
+
+    onCourseSelected(value) {
+        console.log(value);
+
+    }
 
 }
