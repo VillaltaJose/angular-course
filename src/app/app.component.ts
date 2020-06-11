7fd45f601353df8090f5f99a5f80b4acd8e5daf0
Jose Villalta <josemiguelvillaltah@gmail.com>
2020-06-11 10:48:07 -0500

    Services Finished :D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,44 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
+import { Observable } from 'rxjs';
+import { CoursesService } from './services/courses.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
+	courses$: Observable<Course[]>;
+	courses = COURSES;
 
-  courses = COURSES;
+	constructor(
+		private coursesService: CoursesService
+	) {
 
-  constructor() {
+	}
 
-  }
+	ngOnInit() {
 
-  ngOnInit() {
-  }
+		console.log(this.coursesService);
+
+		this.courses$ = this.coursesService.loadCourses();
+
+	}
+
+	save(course: Course) {
+
+		this.coursesService.saveCourse(course)
+			.subscribe(
+				() => { console.log('Curso guardado!') }
+			);
+
+	}
 
 
 
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CoursesService {
+
+	constructor(
+		private http: HttpClient
+	) {
+		console.log("creating coursesService");
+	}
+
+	loadCourses(): Observable<Course[]> {
+		const params = new HttpParams()
+			.set("page", "1")
+			.set("pageSize", "2");
+
+		return this.http.get<Course[]>('/api/courses', {params});
+	}
+
+	saveCourse(course: Course) {
+		const headers = new HttpHeaders()
+			.set("X-Auth", "userId");
+
+		return this.http.put(`/api/courses/${course.id}`, course, {headers});
+	}
+
+}
